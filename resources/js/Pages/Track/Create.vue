62865b57cee9b7aba8b590ffcf70a4e46d139b61
QuentinPacheu <quentin.pacheu@gmail.com>
2024-01-09 09:57:56 +0100

    first

---
+++ resources/js/Pages/Track/Create.vue
@@ -0,0 +1,24 @@
+<template>
+    <MusicLayout>
+        <template #title>
+            Musiques
+        </template>
+        <template #action>
+            <Link :href="route('track.index')">
+                    Retour
+                </Link>
+        </template>
+        <template #content>
+
+        </template>
+    </MusicLayout>
+</template>
+<script>
+import MusicLayout from '@/Layouts/MusicLayout.vue';
+
+export default {
+    components: {
+        MusicLayout
+    }
+}
+</script>
---
+++ resources/js/Pages/Track/Index.vue
@@ -0,0 +1,36 @@
+<template>
+    <MusicLayout>
+        <template #title>
+            Musiques
+        </template>
+        <template #action>
+                <Link :href="route('track.create')">
+                    Créer une musique
+                </Link>
+            </template>
+        <template #content>
+            <div>
+                <ul>
+                    <li v-for="track in tracks"
+                    :key="track.uuid"
+                    :class="[track.id % 2 ? 'text-red-500' : 'text-green-500']">
+                    {{track.title}}
+                    </li>
+                </ul>
+            </div>
+        </template>
+    </MusicLayout>
+</template>
+<script>
+import MusicLayout from '@/Layouts/MusicLayout.vue';
+import { Link } from '@inertiajs/vue3';
+export default {
+    components: {
+        MusicLayout, Link
+    },
+    props: {
+        tracks: Array,
+    }
+
+}
+</script>
