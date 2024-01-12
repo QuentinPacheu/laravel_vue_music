85027499a93ea80820b63cdfd27b29636f8db939
QuentinPacheu <quentin.pacheu@gmail.com>
2024-01-12 16:58:12 +0100

    style V1

--- resources/js/Pages/Playlist/Index.vue
+++ resources/js/Pages/Playlist/Index.vue
@@ -1,27 +1,26 @@
 <template>
     <MusicLayout>
         <template #title>
-            Playlist
+            <h1 class="text-4xl font-bold text-blue-600">Playlist</h1>
         </template>
         <template #action>
-            <Link :href="route('playlists.create')"
-                class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition-all">
+            <Link :href="route('playlists.create')" class="btn-create-playlist">
             Créer une playlist
             </Link>
         </template>
         <template #content>
             <div class="my-4">
                 <input v-model="search" type="search" name="search" id="search"
-                    class="border p-2 rounded-md w-full md:w-2/3 lg:w-1/2 xl:w-1/3"
+                    class="border p-2 rounded-md w-full md:w-2/3 lg:w-1/2 xl:w-1/3 placeholder-gray-500 focus:outline-none focus:ring focus:border-blue-300"
                     placeholder="Rechercher une musique..." />
                 <div class="flex flex-col mt-4 space-y-4">
                     <Track v-for="track in filteredTracks" :key="track.uuid" :track="track"
                         :active="currentTrack == track.uuid" @played="play" @click="playAudio(track)" />
                 </div>
             </div>
-            <div>
+            <div class="playlist-table">
                 <table class="min-w-full border border-gray-300 divide-y divide-gray-300">
-                    <thead class="bg-gray-100">
+                    <thead class="bg-blue-100">
                         <tr>
                             <th class="py-2 px-4 text-left">#</th>
                             <th class="py-2 px-4 text-left">Titre</th>
@@ -36,15 +35,15 @@
                             <td class="py-2 px-4">{{ playlist.title }}</td>
                             <td class="py-2 px-4">{{ playlist.tracks_count }}</td>
                             <td class="py-2 px-4">{{ $moment(playlist.created_at).format('DD/MM/YYYY') }}</td>
-                            <Link :href="route('playlists.show', { playlist: playlist.uuid })"
-                                class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition-all">
-                            Voir
-                            </Link>
-                            <Link
-                            v-if="$page.props.isAdmin"
-                            :href="route('playlists.destroy', { playlist: playlist })" method="delete" as="button">
-                            Supprimer
-                            </Link>
+                            <td class="py-2 px-4">
+                                <Link :href="route('playlists.show', { playlist: playlist.uuid })" class="btn-view">
+                                Voir
+                                </Link>
+                                <Link v-if="$page.props.isAdmin" :href="route('playlists.destroy', { playlist: playlist })"
+                                    method="delete" class="btn-delete" as="button">
+                                Supprimer
+                                </Link>
+                            </td>
                         </tr>
                     </tbody>
                 </table>
@@ -66,5 +65,4 @@ export default {
         playlists: Array,
     },
 };
-
 </script>
--- resources/js/Pages/Track/Index.vue
+++ resources/js/Pages/Track/Index.vue
@@ -1,38 +1,34 @@
 <template>
     <MusicLayout>
         <template #title>
-            Tracks
+            <h1 class="text-4xl font-bold text-blue-600">Tracks</h1>
         </template>
         <template #action>
-            <Link
-            v-if="$page.props.isAdmin"
-                :href="route('track.create')"
-                class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition-all"
-            >
-                Créer une musique
+            <Link v-if="$page.props.isAdmin" :href="route('track.create')" class="btn-create-track">
+            Créer une musique
             </Link>
         </template>
         <template #content>
             <div class="my-4">
-                <input v-model="search" type="search" name="search" id="search" class="border p-2 rounded-md w-full md:w-2/3 lg:w-1/2 xl:w-1/3">
+                <input v-model="search" type="search" name="search" id="search"
+                    class="border p-2 rounded-md w-full md:w-2/3 lg:w-1/2 xl:w-1/3 placeholder-gray-500 focus:outline-none focus:ring focus:border-blue-300"
+                    placeholder="Rechercher une musique..." />
                 <div class="flex flex-col mt-4 gap-4">
-                    <Track v-for="track in filteredTracks" 
-                    :key="track.uuid" 
-                    :track="track" :active="currentTrack == track.uuid" 
-                    @click="playAudio(track)" />
+                    <Track v-for="track in filteredTracks" :key="track.uuid" :track="track"
+                        :active="currentTrack == track.uuid" @click="playAudio(track)" />
                 </div>
                 <div v-if="filteredTracks.length === 0" class="text-center text-gray-600 mt-4">
-                Aucun résultat trouvé.
+                    Aucun résultat trouvé.
                 </div>
             </div>
         </template>
     </MusicLayout>
 </template>
-
+  
 <script>
-    import MusicLayout from "@/Layouts/MusicLayout.vue";
-    import { Link } from "@inertiajs/vue3";
-    import Track from "@/Components/Track/Track.vue";
+import MusicLayout from "@/Layouts/MusicLayout.vue";
+import { Link } from "@inertiajs/vue3";
+import Track from "@/Components/Track/Track.vue";
 
 export default {
     components: {
@@ -43,21 +39,23 @@ export default {
     props: {
         tracks: Array,
     },
-    data(){
-        return{
-            search: '',
+    data() {
+        return {
+            search: "",
             audio: null,
             currentTrack: null,
-        }
+        };
     },
     computed: {
-        filteredTracks(){
-            return this.tracks.filter(track => track.title.toLowerCase().includes(this.search.toLowerCase()));
-        }
+        filteredTracks() {
+            return this.tracks.filter((track) =>
+                track.title.toLowerCase().includes(this.search.toLowerCase())
+            );
+        },
     },
     methods: {
         playAudio(track) {
-            const audioUrl = '/storage/'+ track.music;
+            const audioUrl = "/storage/" + track.music;
             if (!this.currentTrack) {
                 this.audio = new Audio(audioUrl);
                 this.audio.play();
@@ -71,11 +69,10 @@ export default {
                 this.audio.play();
             }
             this.currentTrack = track.uuid;
-            this.audio.addEventListener('ended', () => {
+            this.audio.addEventListener("ended", () => {
                 this.currentTrack = null;
             });
-        }
-    }
-
-}
+        },
+    },
+};
 </script>
